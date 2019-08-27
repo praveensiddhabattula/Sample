7effd5e261635c0f6ae9ef1d004d0a012ebbfbe5
Praveen Siddhabattula <praveen.siddhabattula@gmail.com>
2019-08-27 20:33:55 +0530

    Update print.sh

--- Training/print.sh
+++ Training/print.sh
@@ -9,3 +9,7 @@ echo hi hello big world
 ##how to print 2 lines using echo
 echo hello from app1
 echo hello from app2
+
+##how to print multiple echo in single line
+##\n to print new line
+echo hello from Line1\nHello from Line2
