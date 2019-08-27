9a004dabb19cdec231fc6aed24c08fb426ef76aa
Praveen Siddhabattula <praveen.siddhabattula@gmail.com>
2019-08-28 00:51:51 +0530

    Update Inputs.sh

--- Training/Inputs.sh
+++ Training/Inputs.sh
@@ -8,5 +8,10 @@ read -p 'Enter your course:' course
 
 echo -e "\nhello $name,welcome to $course"
 
+##sometime we need to read some secret content like passwords .
+
+read -s -p "enter your password:' pass
+
+echo -e "\nhello $name,welcome to $course,your password is $pass"
 
 ##Special Variables
