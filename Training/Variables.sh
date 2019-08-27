e1e731fcace4a7e26cbd8b822c6ee7936152e390
Praveen Siddhabattula <praveen.siddhabattula@gmail.com>
2019-08-28 00:09:04 +0530

    Create Variables.sh

---
+++ Training/Variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##If you assign a name to data then that is called as variable
+##VARNAME=DATA
+##Access a variable data by using variable name with $ character .Also you can access variable with {} brackets as well , ${VARNAME}
+
+count=10
+class=Devops
+
+echo "this class is about $class and no. of persons in this class =$count"
+or 
+## echo "this class is abut ${class} and no. of persons in this class is ${count}"
+
